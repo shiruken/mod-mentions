b8d3f62807dff4e54707f996f6244873878526a5
Colin Sullender <shiruken343@gmail.com>
2023-05-24 19:41:26 -0600

    Identify and send modmail on comment mentions

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,50 @@
-import { Devvit } from '@devvit/public-api';
-// Visit developers.reddit.com/docs to view documentation for the Devvit api
+import {
+  Devvit,
+  RedditAPIClient
+ } from '@devvit/public-api';
+import { CommentSubmit, Metadata } from '@devvit/protos';
+
+const reddit = new RedditAPIClient();
+
+Devvit.addTrigger({
+  event: Devvit.Trigger.CommentSubmit,
+  handler: checkCommentModMention
+});
+
+async function checkCommentModMention(event: CommentSubmit, metadata?: Metadata) {
+  const subreddit = await reddit.getSubredditById(String(event.subreddit?.id), metadata);
+  const moderators = [];
+  for await(const moderator of subreddit.getModerators())
+    moderators.push(moderator.username);
+
+  // Check if any subreddit moderators are mentioned in comment text
+  // Not robust, only returns first mention and cannot handle substrings (e.g. u/spez and u/spez_bot)
+  const comment = await reddit.getCommentById(String(event.comment?.id), metadata);
+  const bodyLowerCase = comment.body.toLowerCase();
+  const index = moderators.findIndex(v => bodyLowerCase.includes(v.toLowerCase()));
+
+  if (index >= 0) {
+
+    const moderator = moderators[index];
+    console.log(`${comment.id} mentions u/${moderator}`);
+
+    const permalink = `https://www.reddit.com/r/${subreddit.name}` + 
+                      `/comments/${comment.postId.slice(3)}` +
+                      `/comment/${comment.id.slice(3)}/`;
+
+    const text = `The following comment by u/${comment.authorName} mentions the moderator u/${moderator}:\n\n` +
+                 `**Link:** ${permalink}\n\n` +
+                 `**Body:** ${comment.body}`
+
+    await reddit.sendPrivateMessage(
+      {
+        to: `/r/${subreddit.name}`,
+        subject: "Moderator Mentioned",
+        text: text,
+      },
+      metadata
+    );
+  }
+}
+
 export default Devvit;
