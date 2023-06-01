5d4971fffc008f214bb0a05ca0b2e709601f195d
Colin Sullender <shiruken343@gmail.com>
2023-06-01 14:45:17 -0600

    Report on comment mentions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import {
+  Comment,
   Devvit,
-  RedditAPIClient
+  RedditAPIClient,
+  Subreddit
  } from '@devvit/public-api';
 import { CommentSubmit, Metadata } from '@devvit/protos';
 
 const reddit = new RedditAPIClient();
+const lc = Devvit.use(Devvit.Types.RedditAPI.LinksAndComments);
 
 Devvit.addTrigger({
   event: Devvit.Trigger.CommentSubmit,
@@ -18,7 +21,7 @@ async function checkCommentModMention(event: CommentSubmit, metadata?: Metadata)
     moderators.push(moderator.username);
 
   // Check if any subreddit moderators are mentioned in comment text
-  // Not robust, only returns first mention and cannot handle substrings (e.g. u/spez and u/spez_bot)
+  // Not robust, only returns first mention and cannot handle substrings (e.g. u/spez vs. u/spez_bot)
   const comment = await reddit.getCommentById(String(event.comment?.id), metadata);
   const bodyLowerCase = comment.body.toLowerCase();
   const index = moderators.findIndex(v => bodyLowerCase.includes(v.toLowerCase()));
@@ -28,23 +31,40 @@ async function checkCommentModMention(event: CommentSubmit, metadata?: Metadata)
     const moderator = moderators[index];
     console.log(`${comment.id} mentions u/${moderator}`);
 
+    // Report Comment
+    await reportComment(comment, moderator, metadata);
+
+    // Send Modmail
     const permalink = `https://www.reddit.com/r/${subreddit.name}` + 
                       `/comments/${comment.postId.slice(3)}` +
                       `/comment/${comment.id.slice(3)}/`;
-
     const text = `The following comment by u/${comment.authorName} mentions the moderator u/${moderator}:\n\n` +
                  `**Link:** ${permalink}\n\n` +
                  `**Body:** ${comment.body}`
+    await sendModmail(subreddit, text, metadata);
 
-    await reddit.sendPrivateMessage(
-      {
-        to: `/r/${subreddit.name}`,
-        subject: "Moderator Mentioned",
-        text: text,
-      },
-      metadata
-    );
   }
+}
+
+async function reportComment(comment: Comment, moderator: string, metadata?: Metadata) {
+  await lc.Report(
+    {
+      thingId: comment.id,
+      reason: `Comment mentions the moderator u/${moderator}`
+    },
+    metadata
+  );
+}
+
+async function sendModmail(subreddit: Subreddit, text: string, metadata?: Metadata) {
+  await reddit.sendPrivateMessage(
+    {
+      to: `/r/${subreddit.name}`,
+      subject: "Moderator Mentioned",
+      text: text,
+    },
+    metadata
+  );
 }
 
 export default Devvit;
