d4744f0ab876ec2cd5b941da12cc94805612daea
Colin Sullender <shiruken343@gmail.com>
2023-06-02 19:49:51 -0600

    Consolidate into single trigger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { Devvit, getSetting, RedditAPIClient } from '@devvit/public-api';
-import { CommentSubmit, Metadata, PostSubmit } from '@devvit/protos';
+import { Devvit, RedditAPIClient, Post, Comment, getSetting } from '@devvit/public-api';
+import { Metadata } from '@devvit/protos';
 
 const reddit = new RedditAPIClient();
 const lc = Devvit.use(Devvit.Types.RedditAPI.LinksAndComments);
@@ -66,17 +66,12 @@ Devvit.addSettings([
 ]);
 
 Devvit.addTrigger({
-  event: Devvit.Trigger.PostSubmit,
-  handler: checkPostModMention
+  events: [Devvit.Trigger.PostSubmit, Devvit.Trigger.CommentSubmit],
+  handler: checkModMention,
 });
 
-Devvit.addTrigger({
-  event: Devvit.Trigger.CommentSubmit,
-  handler: checkCommentModMention
-});
-
-async function checkPostModMention(event: PostSubmit, metadata?: Metadata) {
-
+async function checkModMention(event: Devvit.MultiTriggerEvent, metadata?: Metadata) {
+  
   const reportContent = await getSetting('reportContent', metadata) as boolean;
   const lockContent = await getSetting('lockContent', metadata) as boolean;
   const removeContent = await getSetting('removeContent', metadata) as boolean;
@@ -84,64 +79,82 @@ async function checkPostModMention(event: PostSubmit, metadata?: Metadata) {
   const slackWebhook = await getSetting('slackWebhook', metadata) as string;
   const discordWebhook = await getSetting('discordWebhook', metadata) as string;
 
-  if (!reportContent && !lockContent && !removeContent && !modmailContent && !slackWebhook && !discordWebhook)
-    throw new Error('No actions are enabled in app configuration');
+  if (!reportContent && !lockContent && !removeContent && !modmailContent && !slackWebhook && !discordWebhook) {
+    console.error('No actions are enabled in app configuration');
+    return;
+  }
 
   let excludedMods = await getSetting('excludedMods', metadata) as string;
   excludedMods = excludedMods.replace(/(\/?u\/)|\s/g, ""); // Strip out user tags and spaces
   const excludedModsList = excludedMods.toLowerCase().split(",");
-  excludedModsList.push('mod-mentions');
+  excludedModsList.push('mod-mentions'); // Exclude app account
 
   // Get list of subreddit moderators, excluding any defined in configuration
   // Has trouble on subreddits with a large number of moderators (e.g. r/science)
-  const subreddit = await reddit.getSubredditById(String(event.subreddit?.id), metadata);
+  const subreddit = await reddit.getSubredditById(String(event.event.subreddit?.id), metadata);
   const moderators = [];
   for await(const moderator of subreddit.getModerators())
     if (!excludedModsList.includes(moderator.username.toLowerCase()))
       moderators.push(moderator.username);
 
-  // Check if any subreddit moderators are mentioned in post title or text
-  // Not robust, only returns first mention and cannot handle substrings (e.g. u/spez vs. u/spez_bot)
-  const post = await reddit.getPostById(String(event.post?.id), metadata);
-  const textLowerCase = post.title.toLowerCase() + " " + String(post.body).toLowerCase();
-  const index = moderators.findIndex(v => textLowerCase.includes(`u/${v.toLowerCase()}`));
+  let object: Post | Comment;
+  let text: string;
+  let type: string;
+  if (event.type === Devvit.Trigger.PostSubmit) {
+    type = "post";
+    object = await reddit.getPostById(String(event.event.post?.id), metadata);
+    text = object.title + " " + String(object.body);
+  } else if (event.type === Devvit.Trigger.CommentSubmit) {
+    type = "comment";
+    object = await reddit.getCommentById(String(event.event.comment?.id), metadata);
+    text = object.body;
+  } else {
+    console.error('Unexpected trigger type: %d', event.type);
+    return;
+  }
 
+  // Check if any subreddit moderators are mentioned
+  // Not robust, only returns first mention and cannot handle substrings (e.g. u/spez vs. u/spez_bot)
+  text = text.toLowerCase();
+  const index = moderators.findIndex(m => text.includes(`u/${m.toLowerCase()}`));
+
+  // Execute actions and send notifications
   if (index >= 0) {
 
     const moderator = moderators[index];
-    console.log(`${post.id} mentions u/${moderator}`);
-    const permalink = `https://www.reddit.com${post.permalink}`;
-    
-    // Report Post
+    console.log(`${object.id} mentions u/${moderator}`);
+
+    // Report Content
     if (reportContent) {
       await lc.Report(
         {
-          thingId: post.id,
-          reason: `Post mentions the moderator u/${moderator}`
+          thingId: object.id,
+          reason: `Mentions moderator u/${moderator}`
         },
         metadata
       );
-      console.log(`Reported ${post.id}`);
+      console.log(`Reported ${object.id}`);
     }
 
-    // Lock Post
+    // Lock Content
     if (lockContent) {
-      await post.lock();
-      console.log(`Locked ${post.id}`);
+      await object.lock();
+      console.log(`Locked ${object.id}`);
     }
 
-    // Remove Post
+    // Remove Content
     if (removeContent) {
-      await post.remove();
-      console.log(`Removed ${post.id}`);
+      await object.remove();
+      console.log(`Removed ${object.id}`);
     }
-
+    
     // Send Modmail
     if (modmailContent) {
-      const text = `The following post by u/${post.authorName} mentions the moderator u/${moderator}:\n\n` +
-                  `**Link:** https://www.reddit.com${post.permalink}\n\n` +
-                  `**Title:** ${post.title}` +
-                  ((post.body) ? `\n\n**Body:** ${post.body}` : "")
+      const text = `The moderator u/${moderator} has been mentioned in a ${type}:\n\n` +
+                   `* **Link:** https://www.reddit.com${object.permalink}\n\n` +
+                   `* **User:** u/${object.authorName}` +
+                   (('title' in object) ? `\n\n* **Title:** ${object.title}` : "") +
+                   ((object.body) ? `\n\n* **Body:** ${object.body}` : "");
       await reddit.sendPrivateMessage(
         {
           to: `/r/${subreddit.name}`,
@@ -150,18 +163,18 @@ async function checkPostModMention(event: PostSubmit, metadata?: Metadata) {
         },
         metadata
       );
-      console.log(`Sent modmail about ${post.id}`);
+      console.log(`Sent modmail about ${object.id}`);
     }
 
     // Send to Slack
     if (slackWebhook) {
-      const slack_payload = {
+      const slackPayload = {
         blocks: [
           {
             type: "section",
             text: {
               type: "mrkdwn",
-              text: `u/${moderator} has been mentioned in a post:`
+              text: `The moderator <https://www.reddit.com/user/${moderator}|u/${moderator}> has been mentioned in a ${type}:`
             }
           },
           {
@@ -169,10 +182,10 @@ async function checkPostModMention(event: PostSubmit, metadata?: Metadata) {
             elements: [
               {
                 type: "mrkdwn",
-                text: `*Link:* https://www.reddit.com${post.permalink}\n` +
-                      `*User:* <https://www.reddit.com/user/${post.authorName}|u/${post.authorName}>\n` +
-                      `*Title:* ${post.title}` +
-                      ((post.body) ? `\n*Body:* ${post.body}` : "")
+                text: `https://www.reddit.com${object.permalink}\n` +
+                      `*User:* <https://www.reddit.com/user/${object.authorName}|u/${object.authorName}>` +
+                      (('title' in object) ? `\n*Title:* ${object.title}` : "") +
+                      ((object.body) ? `\n*Body:* ${object.body}` : "")
               }
             ]
           }
@@ -180,40 +193,43 @@ async function checkPostModMention(event: PostSubmit, metadata?: Metadata) {
       };
       await fetch(slackWebhook, {
         method: 'POST',
-        body: JSON.stringify(slack_payload)
+        body: JSON.stringify(slackPayload)
       });
-      console.log(`Sent Slack message about ${post.id}`);
+      console.log(`Sent Slack message about ${object.id}`);
     }
 
     // Send to Discord
     if (discordWebhook) {
-      const discord_payload = {
+      const discordPayload = {
         username: "Moderator Mentions",
-        content: `u/${moderator} has been mentioned in a post`,
+        content: `The moderator [u/${moderator}](https://www.reddit.com/user/${moderator}) has been mentioned in a post`,
         embeds: [
           {
+            color: 16711680,
             fields: [
               {
                 name: "Link",
-                value: permalink
+                value: `https://www.reddit.com${object.permalink}`
               },
               {
                 name: "User",
-                value: `[u/${post.authorName}](https://www.reddit.com/user/${post.authorName})`
-              },
-              {
-                name: "Title",
-                value: post.title
+                value: `[u/${object.authorName}](https://www.reddit.com/user/${object.authorName})`
               }
             ]
           }
         ]
       };
 
-      if (post.body)
-        discord_payload.embeds[0].fields.push({
+      if ('title' in object)
+        discordPayload.embeds[0].fields.push({
+          name: "Title",
+          value: object.title
+        });
+
+      if (object.body)
+        discordPayload.embeds[0].fields.push({
           name: "Body",
-          value: post.body
+          value: object.body
         });
 
       await fetch(discordWebhook, {
@@ -221,157 +237,9 @@ async function checkPostModMention(event: PostSubmit, metadata?: Metadata) {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(discord_payload)
+        body: JSON.stringify(discordPayload)
       });
-      console.log(`Sent Discord message about ${post.id}`);
-    }
-
-  }
-}
-
-async function checkCommentModMention(event: CommentSubmit, metadata?: Metadata) {
-
-  const reportContent = await getSetting('reportContent', metadata) as boolean;
-  const lockContent = await getSetting('lockContent', metadata) as boolean;
-  const removeContent = await getSetting('removeContent', metadata) as boolean;
-  const modmailContent = await getSetting('modmailContent', metadata) as boolean;
-  const slackWebhook = await getSetting('slackWebhook', metadata) as string;
-  const discordWebhook = await getSetting('discordWebhook', metadata) as string;
-
-  if (!reportContent && !lockContent && !removeContent && !modmailContent && !slackWebhook && !discordWebhook)
-    throw new Error('No actions are enabled in app configuration');
-
-  let excludedMods = await getSetting('excludedMods', metadata) as string;
-  excludedMods = excludedMods.replace(/(\/?u\/)|\s/g, ""); // Strip out user tags and spaces
-  const excludedModsList = excludedMods.toLowerCase().split(",");
-  excludedModsList.push('mod-mentions');
-
-  // Get list of subreddit moderators, excluding any defined in configuration
-  // Has trouble on subreddits with a large number of moderators (e.g. r/science)
-  const subreddit = await reddit.getSubredditById(String(event.subreddit?.id), metadata);
-  const moderators = [];
-  for await(const moderator of subreddit.getModerators())
-    if (!excludedModsList.includes(moderator.username.toLowerCase()))
-      moderators.push(moderator.username);
-
-  // Check if any subreddit moderators are mentioned in comment text
-  // Not robust, only returns first mention and cannot handle substrings (e.g. u/spez vs. u/spez_bot)
-  const comment = await reddit.getCommentById(String(event.comment?.id), metadata);
-  const bodyLowerCase = comment.body.toLowerCase();
-  const index = moderators.findIndex(v => bodyLowerCase.includes(`u/${v.toLowerCase()}`));
-
-  if (index >= 0) {
-
-    const moderator = moderators[index];
-    console.log(`${comment.id} mentions u/${moderator}`);
-
-    const permalink = `https://www.reddit.com/r/${subreddit.name}` + 
-                      `/comments/${comment.postId.slice(3)}` +
-                      `/comment/${comment.id.slice(3)}/`;
-    
-    // Report Comment
-    if (reportContent) {
-      await lc.Report(
-        {
-          thingId: comment.id,
-          reason: `Comment mentions the moderator u/${moderator}`
-        },
-        metadata
-      );
-      console.log(`Reported ${comment.id}`);
-    }
-
-    // Lock Comment
-    if (lockContent) {
-      await comment.lock();
-      console.log(`Locked ${comment.id}`);
-    }
-
-    // Remove Comment
-    if (removeContent) {
-      await comment.remove();
-      console.log(`Removed ${comment.id}`);
-    }
-
-    // Send Modmail
-    if (modmailContent) {
-      const text = `The following comment by u/${comment.authorName} mentions the moderator u/${moderator}:\n\n` +
-                  `**Link:** ${permalink}\n\n` +
-                  `**Body:** ${comment.body}`
-      await reddit.sendPrivateMessage(
-        {
-          to: `/r/${subreddit.name}`,
-          subject: "Moderator Mentioned",
-          text: text,
-        },
-        metadata
-      );
-      console.log(`Sent modmail about ${comment.id}`);
-    }
-
-    // Send to Slack
-    if (slackWebhook) {
-      const slack_payload = {
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `u/${moderator} has been mentioned in a comment:`
-            }
-          },
-          {
-            type: "context",
-            elements: [
-              {
-                type: "mrkdwn",
-                text: `*Link:* ${permalink}\n` +
-                      `*User:* <https://www.reddit.com/user/${comment.authorName}|u/${comment.authorName}>\n` +
-                      `*Body:* ${comment.body}`
-              }
-            ]
-          }
-        ]
-      };
-      await fetch(slackWebhook, {
-        method: 'POST',
-        body: JSON.stringify(slack_payload)
-      });
-      console.log(`Sent Slack message about ${comment.id}`);
-    }
-  
-    // Send to Discord
-    if (discordWebhook) {
-      const discord_payload = {
-        username: "Moderator Mentions",
-        content: `u/${moderator} has been mentioned in a comment`,
-        embeds: [
-          {
-            fields: [
-              {
-                name: "Link",
-                value: permalink
-              },
-              {
-                name: "User",
-                value: `[u/${comment.authorName}](https://www.reddit.com/user/${comment.authorName})`
-              },
-              {
-                name: "Body",
-                value: comment.body
-              }
-            ]
-          }
-        ]
-      };
-      await fetch(discordWebhook, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(discord_payload)
-      });
-      console.log(`Sent Discord message about ${comment.id}`);
+      console.log(`Sent Discord message about ${object.id}`);
     }
   }
 }
